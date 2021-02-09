58c5292ff7d3a30f5864e7a9bdca4d61ac0c0074
djvirux <jcvirus64@hotmail.com>
2021-02-09 19:56:51 +0100

    Punto 3 encapsulamiento de atributos

--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -7,14 +7,15 @@ public class Main {
         double saldoActual;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es "+ saldoActual );
-
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(cuenta1, 0);
     }
 
-	private static void operativa_cuenta(CCuenta cuenta1) {
-		try {
+	private static void operativa_cuenta(CCuenta cuenta1, float catidad) {
+		double saldoActual;
+		saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+
+        try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
